076ea3e38f0ea229f4938ea98db6748e37e18fc2
emanherawy <eman.herawy@gmail.com>
2020-03-28 01:35:40 +0200

    work in global style

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
 import * as Font from 'expo-font'
+import {GlobalStyles} from './constants/globalStyles'
+
 import { AppLoading } from 'expo'
 const getFotns=()=>Font.loadAsync({
   "rosario-italic":require("./assets/fonts/Rosario-Italic-VariableFont_wght.ttf"),
@@ -10,8 +12,8 @@ export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
   if(fontsLoaded){
     return (
-      <View style={styles.container}>
-        <Text style={styles.textItem}>Open up App.js to start working on your app!</Text>
+      <View style={GlobalStyles.container}>
+        <Text style={GlobalStyles.textItem}>Open up App.js to start working on your app!</Text>
       </View>
     );
   }else{
@@ -26,15 +28,3 @@ export default function App() {
 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  textItem:{
-    fontFamily:'rosario',
-
-  }
-});
