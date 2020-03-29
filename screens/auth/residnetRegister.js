78fcbb331cf3641cda129596622a5231c62441f4
emanherawy <eman.herawy@gmail.com>
2020-03-29 07:33:48 +0200

    integrate with blockchain

--- screens/auth/residnetRegister.js
+++ screens/auth/residnetRegister.js
@@ -1,47 +1,51 @@
 import React from 'react'
-import { StyleSheet,  Button, View } from 'react-native'
+import { StyleSheet,   View } from 'react-native'
 import Card from '../../shared/card'
 import Thumbnail from '../../shared/thumbnail'
 import HyperLink from '../../shared/hyperlink'
 import { globalStyles } from '../../styles/global'
 import ResidentRegisterForm from '../../components/residentRegisterForm'
+
+import UserFactoryService from '../../service/UserFactory.js';
+import Web3Service from '../../service/web3.service.js';
+import LocalStorageService from '../../service/core/LocalStorage.service'
+
+const factoryAddress = "0x45e00820163F3d1bb6Df0781666d90b4B29C26c9"
 const ResidnetRegister = ({navigation}) => {
     const Url = require('../../assets/images/logo2.jpg');
 
     const handleRegister = async(value) => {
-/**
- *    userWallet = new WalletService();
-    web3Service = new Web3Service();
- *         // create addresss
-        //  await this.createAccount(this.state.password);
+
+ const web3Service = new Web3Service();
+        // create addresss
+        //  await  createAccount( state.password);
         // save to blockchain
-        //   const address = await this.web3Service.createAccount(this.state.password);
-        const address = await this.web3Service.getAccountByIndex(2);
-        this.setState({ ethAddress: address })
-        //   this.setState({ethAddress:address.address,privateKey:address.privateKey})
-        //   const factory = new UserFactoryService(address.privateKey);
-        const factory = new UserFactoryService();
+          const address = await  web3Service.createAccount( value.password);
+          value.ethAddress= address.address;
+          value.privateKey=address.privateKey;
+          const factory = new UserFactoryService(address.privateKey);
+        // const factory = new UserFactoryService();
         console.log(factory, 'factory');
 
 
-        const tx = await factory.addResident(this.state.ethAddress, this.state.ethAddress, 2000000000, factoryAddress);
+        const tx = await factory.addResident( value.ethAddress,  value.ethAddress, 2000000000, factoryAddress);
         //   console.log(tx,'tx');
 
-        // const tx=  await factory. addResidentSigned(this.state.ethAddress, this.state.ethAddress, this.state.privateKey, 2000000000,factoryAddress)
+        // const tx=  await factory. addResidentSigned( state.ethAddress,  state.ethAddress,  state.privateKey, 2000000000,factoryAddress)
         console.log(tx, 'tx');
         if (tx) {
             // save to off chain storage
-            // const wallet = await factory.getResident(this.state.ethAddress,factoryAddress);
-            this.setState({ walletAddress: tx.address });
+            // const wallet = await factory.getResident( state.ethAddress,factoryAddress);
+            value. walletAddress= tx.address ;
             const storage = new LocalStorageService();
-            await storage.setUser(this.state);
-            console.log(this.state);
-            console.log(this.props, 'props');
+            await storage.setUser( value);
+            console.log( value);
+            console.log( props, 'props');
 
 
-            //Api.registerRes(this.state)
+            //Api.registerRes( state)
         }
- */
+
           
           
     };
