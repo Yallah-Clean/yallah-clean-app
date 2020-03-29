a9fdc054bbb479f1dc9f35de6d1db2bf5dfaf6e1
emanherawy <eman.herawy@gmail.com>
2020-03-29 05:58:50 +0200

    add registeration type design and logic

--- screens/auth/registerType.js
+++ screens/auth/registerType.js
@@ -1,14 +1,37 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text,TouchableOpacity, View } from 'react-native'
+import Thumbnail from '../../shared/thumbnail'
+import { globalStyles } from '../../styles/global'
 
-const RegisterType = () => {
+const RegisterType = ({navigation}) => {
+    const collectorImage = require('../../assets/images/collector.jpg');
+    const residnetImage = require('../../assets/images/residnet.jpg');
+    const navigate=(value)=>{
+        if(value==1){
+            navigation.navigate("ResidnetRegister")
+        }else if (value==2){
+            navigation.navigate("CollectorRegister")
+        }
+
+    }
     return (
-        <View>
-            <Text>Register Type View</Text>
-        </View>
+        <View style={globalStyles.container}>
+            <TouchableOpacity onPress={()=>navigate(1)} style={globalStyles.container}>
+            <Thumbnail source={residnetImage} ></Thumbnail>
+
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>navigate(2)} style={globalStyles.container}>
+            <Thumbnail source={collectorImage} ></Thumbnail>
+
+            </TouchableOpacity>
+
+        
+    </View>
     )
 }
 
 export default RegisterType
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  
+})
