72d98a7e251d1c6ee6af872e85500f43559d8799
emanherawy <eman.herawy@gmail.com>
2020-03-31 01:41:53 +0200

    fix ux

--- shared/card.js
+++ shared/card.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 export default function Card(props) {
   return (
     <View style={styles.card}>
-      <View style={styles.cardContent}>
-        { props.children }
-      </View>
+      <View style={styles.cardContent}>{props.children}</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
+    flex: 2,
     borderRadius: 6,
     elevation: 3,
     backgroundColor: '#fff',
-    shadowOffset: { width: 1, height: 1 },
+    shadowOffset: {width: 1, height: 1},
     shadowColor: '#333',
     shadowOpacity: 0.3,
     shadowRadius: 2,
@@ -25,6 +24,9 @@ const styles = StyleSheet.create({
   },
   cardContent: {
     marginHorizontal: 18,
-    marginVertical: 20,
-  }
+    marginVertical: 10,
+
+    // paddingBottom:10,
+    flex: 1,
+  },
 });
