875d7f4db70d30093133e8e7356d70c43211ff3a
emanherawy <eman.herawy@gmail.com>
2020-03-31 01:44:10 +0200

    basic design for profile and wallet

--- screens/wallet.js
+++ screens/wallet.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Card from '../shared/card'
+import FlatButton from '../shared/button'
+import { globalStyles } from '../styles/global'
 
 const Wallet = () => {
     return (
-        <View>
-            <Text>Wallet View</Text>
-        </View>
+     <View style={globalStyles.container}>
+            <Card>
+<Text>Current Token in wallet : 50</Text>
+<Text>Total Token in wallet : 150</Text>
+<FlatButton text="Redeem"/>
+
+        </Card>
+     </View>
     )
 }
 
